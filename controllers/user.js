06a6529ec38e1ed38bd111b78e8e8dfa6dc603fa
ivanzapata97 <ivanzapata.f@gmail.com>
2019-11-29 09:54:50 -0300

    Registro de usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,8 @@
 'use strict'
 
 var validator = require('validator')
+var bcrypt = require('bcrypt-nodejs')
+var User = require('../models/user')
 
 var controller = {
     probando: function(req, res) {
@@ -26,28 +28,63 @@ var controller = {
         var validate_password = !validator.isEmpty(params.password)
 
         if (validate_name && validate_surname && validate_email && validate_password){
-            
+            //crear objeto de usuario
+            var user = new User()
+
+            //asignar valores al usuario
+            user.name = params.name
+            user.surname = params.surname
+            user.email = params.email.toLowerCase()
+            user.role = 'ROLE_USER'
+            user.image = null
+
+            //comprobar si el usuario existe
+            User.findOne({email:user.email}, (err, issetUser) => {
+                if(err){
+                    return res.status(500).send({
+                        message:'Error al comprobar duplicidad de usuario'
+                    })
+                }
+                if(!issetUser){
+                    //si no existe, cifrar la constraseña
+                    bcrypt.hash(params.password, null, null, (err, hash) => {
+                        user.password = hash
+
+                        //guardar usuario
+                        user.save((err, userStored) => {
+                            if(err){
+                                return res.status(200).send({
+                                    message:'Error al guardar el usuario'
+                                })
+                            }
+                            if(!userStored){
+                                return res.status(200).send({
+                                    message:'El usuario no se ha guardado'
+                                })
+                            } else {
+                                return res.status(200).send({
+                                    status:'success',
+                                    user: userStored
+                                })
+                            }
+                        })
+                        //devolver respuesta
+                        return res.status(200).send({
+                            message:'Usuario no estaba registrado'
+                        })
+                    })   
+                } else {
+                    return res.status(200).send({
+                        message:'El usuario ya esta registrado'
+                    })
+                }
+            })
+           
         } else {
             return res.status(200).send({
                 message: 'validación de los datos del usuario incorrecta, intentalo de nuevo'
             })
         }
-
-        //crear objeto de usuario
-
-        //asignar valores al usuario
-
-        //comprobar si el usuario existe
-
-        //si no existe, cifrar la constraseña
-
-        //guardar usuario
-
-        //devolver respuesta
-
-        return res.status(200).send({
-            message:'registro de usuarios'
-        })
     }
 }
 
